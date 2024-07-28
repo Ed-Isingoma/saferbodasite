0a858c113b857bed3c832f8d52f54969595845b8
Tina <tinaamarion@gmail.com>
2024-07-28 15:27:53 +0300

    changes made

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -1,7 +1,8 @@
 let slideIndex = 0;
+showSlides();
 
 function showSlides() {
-    const slides = document.getElementsByClassName("slide");
+    let slides = document.getElementsByClassName("slide");
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
@@ -10,7 +11,3 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block";  
     setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-    showSlides();
-});
