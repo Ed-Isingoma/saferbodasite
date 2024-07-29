30c8baeeb9b73acb1e98a3538f6b90b41699dd63
Tina <tinaamarion@gmail.com>
2024-07-30 00:24:45 +0300

    changes made

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -1,13 +1,42 @@
-let slideIndex = 0;
-showSlides();
+// Main Image Slideshow
+let mainSlideIndex = 0;
+showMainSlides();
 
-function showSlides() {
+function showMainSlides() {
     let slides = document.getElementsByClassName("slide");
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    slides[slideIndex-1].style.display = "block";  
-    setTimeout(showSlides, 3000); // Change image every 3 seconds
+    mainSlideIndex++;
+    if (mainSlideIndex > slides.length) {mainSlideIndex = 1}    
+    slides[mainSlideIndex - 1].style.display = "block";  
+    setTimeout(showMainSlides, 3000); // Change image every 3 seconds
+}
+
+// Customer Testimonials Slideshow
+let testimonialSlideIndex = 1;
+showTestimonialSlides(testimonialSlideIndex);
+
+function changeSlide(n) {
+    showTestimonialSlides(testimonialSlideIndex += n);
+}
+
+function currentSlide(n) {
+    showTestimonialSlides(testimonialSlideIndex = n);
+}
+
+function showTestimonialSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("testimonial-slide");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {testimonialSlideIndex = 1}    
+    if (n < 1) {testimonialSlideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[testimonialSlideIndex - 1].style.display = "block";  
+    dots[testimonialSlideIndex - 1].className += " active";
 }
